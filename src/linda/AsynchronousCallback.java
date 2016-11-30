2aa054182b9a15b37366beb278772661753ea867
sligoo <sacha.liguori@icloud.com>
2016-11-30 11:00:58 +0100

    Modification structure

--- src/linda/AsynchronousCallback.java
+++ src/linda/AsynchronousCallback.java
@@ -14,10 +14,6 @@ public class AsynchronousCallback implements Callback {
     /** Asynchronous call: the associated callback is concurrently run and this one immediately returns.
      * */
     public void call(final Tuple t) {
-        new Thread() {
-            public void run() {
-                cb.call(t);
-            }
-        }.start();
+        new Thread(() -> cb.call(t)).start();
     }
 }
--- src/linda/server/LindaClient.java
+++ src/linda/server/LindaClient.java
@@ -4,6 +4,8 @@ import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 
+import java.util.Collection;
+
 /** Client part of a client/server implementation of Linda.
  * It implements the Linda interface and propagates everything to the server it is connected to.
  * */
@@ -15,7 +17,52 @@ public class LindaClient implements Linda {
     public LindaClient(String serverURI) {
         // TO BE COMPLETED
     }
-    
+
+    @Override
+    public void write(Tuple t) {
+
+    }
+
+    @Override
+    public Tuple take(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Tuple read(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Tuple tryTake(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Tuple tryRead(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Collection<Tuple> takeAll(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Collection<Tuple> readAll(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+
+    }
+
+    @Override
+    public void debug(String prefix) {
+
+    }
+
     // TO BE COMPLETED
 
 }
