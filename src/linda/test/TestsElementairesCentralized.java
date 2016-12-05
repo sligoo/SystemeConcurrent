e15d33ce206464742c041cdfc7a858df5e0aa845
sligoo <sacha.liguori@icloud.com>
2016-12-05 15:25:20 +0100

    ajout tests: take read tryTake tryRead

--- src/linda/test/TestsElementairesCentralized.java
+++ src/linda/test/TestsElementairesCentralized.java
@@ -6,8 +6,13 @@ import linda.shm.CentralizedLinda;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import sun.net.www.ApplicationLaunchException;
 
 import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Project: SystemesConcurrents
@@ -28,10 +33,81 @@ public class TestsElementairesCentralized {
         linda.write(tupleWritten);
         Tuple tupleRead = linda.tryRead(new Tuple(String.class, Integer.class,
                     Integer.class, Boolean.class));
-        Assert.assertTrue(tupleRead.matches(tupleWritten));
+        assertTrue(tupleRead.matches(tupleWritten));
 
         tupleWritten = new Tuple(new Tuple("shittyFlute", 3456), "troubalourds",
                 "japan7", true);
+    }
+
+    @Test
+    public void testTryTake(){
+        Tuple tupleWritten = new Tuple("empty");
+        linda.write(tupleWritten);
+        Assert.assertNull("tryTake a none existing object return null", linda
+                .tryTake(new Tuple(5)));
+
+
+        assertEquals("tryTake a Class tuple template",tupleWritten, linda.tryTake
+                (new Tuple(String.class)));
+
+        tupleWritten = new Tuple(42);
+        linda.write(tupleWritten);
+        assertEquals("tryTake a value tuple template", tupleWritten,linda.tryTake
+                (new Tuple(42)));
+    }
+
+    @Test
+    public void testTake(){
+        Tuple tupleWritten = new Tuple(42);
+        linda.write(tupleWritten);
+        assertTrue("Take a value tuple template", linda.take
+                (new Tuple(42)).matches(tupleWritten));
+        assertNull("After a take, tuple is empty",linda.tryRead(tupleWritten));
+
+    }
+
+    @Test
+    public void testTakeLock() throws InterruptedException {
+        Tuple tupleWritten = new Tuple("empty");
+        linda.write(tupleWritten);
+        Thread mainRunner = new Thread(() -> {
+            linda.take(new Tuple(5));
+        });
+
+        mainRunner.start();
+
+        Thread.sleep(1000);
+
+        assertEquals(Thread.State.WAITING, mainRunner.getState());
+        mainRunner.interrupt();
+
+    }
+
+    @Test
+    public void testReadLock() throws InterruptedException {
+        Tuple tupleWritten = new Tuple("empty");
+        linda.write(tupleWritten);
+        Thread mainRunner = new Thread(() -> {
+            linda.read(new Tuple(5));
+        });
+
+        mainRunner.start();
+
+        Thread.sleep(1000);
+
+        assertEquals(Thread.State.WAITING, mainRunner.getState());
+        mainRunner.interrupt();
+
+    }
+
+    @Test
+    public void testRead(){
+        Tuple tupleWritten = new Tuple(42);
+        linda.write(tupleWritten);
+        assertTrue("Read a value tuple template", linda.read(new Tuple(42)).matches(tupleWritten));
+        assertTrue("After a read, tuple is as before", linda.tryRead(tupleWritten)
+                .matches(tupleWritten));
+
     }
 
 
