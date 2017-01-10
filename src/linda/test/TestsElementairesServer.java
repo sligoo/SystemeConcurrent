5094fd20155671dd0bb56e503d5d83cf91517724
Lucien Haurat <lucien@haur.at>
2017-01-11 00:14:10 +0100

    fini

--- src/linda/test/TestsElementairesServer.java
+++ src/linda/test/TestsElementairesServer.java
@@ -78,7 +78,7 @@ public class TestsElementairesServer {
 
         for(Method m : methods)
         {
-            if (m.getName().startsWith("test") && m.getName() != "testEvents" && !m.getName().contains("Lock")) {
+            if (m.getName().startsWith("test") && !m.getName().contains("Lock")) {
                 try {
                     restartServer();
                     LindaClient client = new LindaClient(URI);
@@ -127,6 +127,11 @@ public class TestsElementairesServer {
         Thread readTester = new Thread(new MultiClientTester(URI, "read", new Tuple(0)));
 
         readTester.start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         assertEquals(Thread.State.WAITING, readTester.getState());
 
         writeTester.start();
@@ -134,140 +139,17 @@ public class TestsElementairesServer {
             Thread.sleep(500);
         } catch (InterruptedException e) {}
         assertEquals(Thread.State.RUNNABLE, readTester.getState());
-    }
 
-    @Test
-    public void testEvents() {
-        int expectedReadImmediate = 3, expectedTakeImmediate = 2, expectedReadFuture = 1, expectedTakeFuture = 1;
-
-        readImmediate = 0;
-        takeImmediate = 0;
-        readFuture = 0;
-        takeFuture = 0;
-
-        LindaClient client = new LindaClient(URI);
-
-        Tuple tuplesImmediate[] = new Tuple[] {
-                new Tuple(345, 762, "coucou", true),
-                new Tuple(53782, 65442, "allo", "test"),
-                new Tuple(345, 762, 7863853, false),
-                new Tuple(53782, 65442, new ArrayList<>(), true),
-                new Tuple(345, "test", "coucou", true),
-                new Tuple("allo", 65442, "coucou", true)
-        };
-
-        for (Tuple tuple : tuplesImmediate) {
-            client.write(tuple);
-        }
-
-        // Tests for Immediate/Read
-        for (int i = 0; i < 2; i++) {
-            client.eventRegister(Linda.eventMode.READ, Linda.eventTiming.IMMEDIATE,
-                    new Tuple(Integer.class, Integer.class, String.class, Boolean.class),
-                    t -> {
-                        if (t.matches(tuplesImmediate[0])) {
-                            readImmediate++;
-                        }
-                    }
-            );
-        }
-
-        Tuple tupleImmediateRead = new Tuple(345, 762, "coucou", 43722);
-        client.eventRegister(Linda.eventMode.READ, Linda.eventTiming.IMMEDIATE,
-                new Tuple(Integer.class, Integer.class, String.class, Integer.class),
-                t -> {
-                    if (t.matches(tupleImmediateRead)) {
-                        readImmediate++;
-                    }
-                }
-        );
         try {
-            Thread.sleep(500);
-            client.write(tupleImmediateRead);
+            writeTester.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        // Tests for Immediate/Take
-        for (int i = 0; i < 2; i++) {
-            client.eventRegister(Linda.eventMode.TAKE, Linda.eventTiming.IMMEDIATE,
-                    new Tuple(Integer.class, Integer.class, String.class, String.class),
-                    t -> {
-                        if (t.matches(tuplesImmediate[1])) {
-                            takeImmediate++;
-                        }
-                    }
-            );
-        }
-
-        Tuple tupleImmediateWrite = new Tuple(345, 762, 43722);
-        client.eventRegister(Linda.eventMode.READ, Linda.eventTiming.IMMEDIATE,
-                new Tuple(Integer.class, Integer.class, Integer.class),
-                t -> {
-                    if (t.matches(tupleImmediateWrite)) {
-                        takeImmediate++;
-                    }
-                }
-        );
         try {
-            Thread.sleep(500);
-            client.write(tupleImmediateRead);
+            readTester.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        // Tests for Future/Read
-        Tuple tupleFutureRead = new Tuple(345, 9543, "coucou", true);
-        client.eventRegister(Linda.eventMode.READ, Linda.eventTiming.FUTURE,
-                new Tuple(Integer.class, Integer.class, String.class, Boolean.class),
-                t -> {
-                    if (t.matches(tupleFutureRead)) {
-                        readFuture++;
-                    }
-                }
-        );
-        try {
-            Thread.sleep(500);
-            client.write(tupleFutureRead);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Assert.assertNotNull("Event for Future/Read shouldn't destroy the tuple",
-                client.tryRead(tupleFutureRead));
-
-        // Tests for Future/Take
-        Tuple tupleFutureTake = new Tuple(34543, 76322, 7853, false, true);
-        client.eventRegister(Linda.eventMode.TAKE, Linda.eventTiming.FUTURE,
-                new Tuple(Integer.class, Integer.class, Integer.class, Boolean.class, Boolean.class),
-                t -> {
-                    if (t.matches(tupleFutureTake)) {
-                        takeFuture++;
-                    }
-                }
-        );
-        try {
-            Thread.sleep(500);
-            client.write(tupleFutureTake);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Assert.assertNull("Event for Future/Take should destroy the tuple", client.tryRead(tupleFutureTake));
-
-        // Checks
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        Assert.assertEquals("Immediate/Read is not working correctly",
-                expectedReadImmediate, readImmediate);
-        Assert.assertEquals("Immediate/Take is not working correctly",
-                expectedTakeImmediate, takeImmediate);
-        Assert.assertEquals("Future/Read is not working correctly",
-                expectedReadFuture, readFuture);
-        Assert.assertEquals("Future/Take is not working correctly",
-                expectedTakeFuture, takeFuture);
     }
 
     public static void main(String[] args) {
